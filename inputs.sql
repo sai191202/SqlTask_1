52f994542b06b01f600f988a185101d944d391f2
sai191202 <pellurusai22@gmail.com>
2025-03-09 16:17:40 +0530

    Create inputs.sql

---
+++ inputs.sql
@@ -0,0 +1,11 @@
+INSERT INTO Employees (EmpID, Name, DeptID) VALUES
+(1, 'Alice', 10),
+(2, 'Bob', 20),
+(3, 'Charlie', 30),
+(4, 'David', NULL);
+
+INSERT INTO Departments (DeptID, DeptName) VALUES
+(10, 'HR'),
+(20, 'IT'),
+(30, 'Finance'),
+(40, 'Sales');
